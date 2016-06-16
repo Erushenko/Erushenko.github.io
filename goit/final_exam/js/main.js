93f009d2c244174b797d55a370eb423a11374ef4
Erushenko <Erushenko@gmail.com>
2016-06-16 19:13:11 +0300

    add exam

--- goit/final_exam/js/main.js
+++ goit/final_exam/js/main.js
@@ -13,15 +13,15 @@
 
             for (var i = 0; i < 7; i++) {
               var imageUrl = data.hits[i].webformatURL;
+              $('.grid-item--'+i)
+                .css({
+                  'backgroundImage': 'url(' + imageUrl +')',
+                  'backgroundSize': 'cover',
+                  // 	'backgroundRepeat': 'no-repeat',
+                  // 	'backgroundPosition': 'top center'
+                })
+                .children(".title").text(data.hits[i].tags);
 
-
-              $('.grid-item--'+i).css({
-                'backgroundImage': 'url(' + imageUrl +')',
-                'backgroundSize': 'cover',
-                // 	'backgroundRepeat': 'no-repeat',
-                // 	'backgroundPosition': 'top center'
-              });
-        
             }
 
 
@@ -35,14 +35,14 @@
 
     refreshGrid();
 
-    $('#search').on('click', function (e) {
+    $('#search__link').on('click', function (e) {
       var r = $('.search__field').val();
       refreshGrid(r);
     });
 
     $('#search').on('keyup', function(e) {
       if (e.keyCode === 13) {
-        event.preventDefault();
+        // event.preventDefault();
         $('#search__link').click();
         return false;
       }
